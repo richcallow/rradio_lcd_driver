70193772abbbff5775511f03121c7a92eaf3f7e1
richcallow <dummy@nowhere.com>
2020-10-24 17:09:34 +0100

    Reading messages over TCP and displaying in decimal form

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::Read;
+
 fn main() {
     println!("Hello, world!");
+    let mut connection =
+        std::net::TcpStream::connect((std::net::Ipv4Addr::LOCALHOST, 8002)).unwrap();
+
+    loop {
+        let mut message_length_buffer = [0; 2];
+
+        match connection.read(&mut message_length_buffer).unwrap() {
+            0 => break,
+            2 => (),
+            _ => panic!("Weird number of bytes read"),
+        }
+
+        let message_length = u16::from_be_bytes(message_length_buffer);
+
+        let mut buffer = vec![0; message_length as usize];
+
+        connection.read_exact(&mut buffer).unwrap();
+
+        println!("length {},   {:?}", message_length, buffer);
+    }
 }
